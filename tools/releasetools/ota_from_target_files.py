52d34fc0154e8ae8ad654b08c940e8c7707c86a2
Prema Chand Alugu <premaca@gmail.com>
2017-03-25 19:56:05 +0530

    Magisk as default and define explict rootless method [1/2]
    WITH_ROOT := true — any other value will not install root in your device.
    Use the above method explicitly in the device for root.

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -813,26 +813,21 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
-  script.ShowProgress(0.2, 10)
-  device_specific.FullOTA_InstallEnd()
-
-  if OPTIONS.extra_script is not None:
-    script.AppendExtra(OPTIONS.extra_script)
-
-  script.UnmountAll()
-  
-  # Install Magisk.
-  
+  if OPTIONS.info_dict.get("WITH_ROOT") == "true":
+#Magisk
   script.Print("Extracting Magisk...");
   script.AppendExtra('package_extract_dir("install/magisk", "/tmp/magisk");')
   script.AppendExtra('run_program("/sbin/busybox", "unzip", "/tmp/magisk/Magisk.zip", "META-INF/com/google/android/update-binary", "-d", "/tmp/magisk");')
   script.Print("Installing Magisk...");
   script.AppendExtra('run_program("/sbin/busybox", "sh", "/tmp/magisk/META-INF/com/google/android/update-binary", "null", "1", "/tmp/magisk/Magisk.zip");')
-
   script.Print("Cleaning up...");
   script.AppendExtra('delete_recursive("/tmp/magisk");')
-
-  script.Print("Installation complete!");
+  script.Print("Installation Complete!");
+else:
+    script.Print(" ")
+    script.Print("ROM does not have any root methid")
+    script.Print(" ")
+    script.Print("Installation complete!");
 
   if OPTIONS.wipe_user_data:
     script.ShowProgress(0.1, 10)

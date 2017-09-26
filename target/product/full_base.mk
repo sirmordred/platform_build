69ac4206c3afa766f5f85a10dab676a87a6d8763
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-26 17:32:44 +0200

    (2/2) Remove Google audio sounds
    A lot of them are too low, i don't like it, keep stock sounds for now
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,14 +39,14 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Zen.ogg \
-    ro.config.notification_sound=Chime.ogg
+    ro.config.ringtone=Ring_Synth_04.ogg \
+    ro.config.notification_sound=pixiedust.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
